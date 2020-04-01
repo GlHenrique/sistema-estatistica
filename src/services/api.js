333c07c6b59776138317908fed1af4d39fe680c1
Glenio Carvalho <gleniohenrique-carvalho@hotmail.com>
2020-04-01 11:01:48 -0300

    Set env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'http://localhost:3333'
-});
+let api;
+
+if (process.env.NODE_ENV === 'production') {
+    api = axios.create({
+        baseURL: 'https://lookup-api.herokuapp.com'
+    });
+} else {
+    api = axios.create({
+        baseURL: 'http://localhost:3333'
+    });
+}
+
 
 export default api;
